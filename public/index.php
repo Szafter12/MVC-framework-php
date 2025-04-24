0015504184eda6d7f0a61c3705bbb5d5dd4fa328
Jakub <jakubpachut@gmail.com>
2025-04-25 01:41:34 +0200

    adding login and register routes

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use app\controllers\AuthController;
 use app\core\Application;
 use app\controllers\SiteController;
 
@@ -12,5 +13,11 @@ $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
 
 $app->router->post('/contact', [SiteController::class, 'handleForm']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
